7f26ae50ae4bbe5d56770739645f6752dd8bd0cd
zhaofeng-shu33 <616545598@qq.com>
2019-08-26 16:24:15 +0800

    add multi-process support for experiment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Two types of graph are used: Gaussian and Two-Level graph.
+
+## Multi-processing
+```shell
+python alg_speed.py --num_times=2 --node_size=200 --method=dt --total_times --multi_thread
+```
+The multi-processing mode can speed up the program a lot. Suppose your computer has n cores and 
+you average the results n times for each algorithm and input data configuration ( to get the
+expectation ), then You can get n times speed up since each running is indepedent with each other. 
--- alg_speed.py
+++ alg_speed.py
@@ -15,12 +15,13 @@ from sklearn.metrics.pairwise import pairwise_kernels
 from info_cluster import InfoCluster
 TOLERANCE = 1e-10
 GAUSSIAN_NODE_LIST = [100, 200, 300, 400, 500]
+TWO_LEVEL_CONFIG_LIST = [3, 4, 5, 6, 7]
 METHOD_LIST = ['dt', 'pdt', 'psp_i', 'pdt_r']
 time_str = datetime.now().strftime('%Y-%m-%d-')
 LOGGING_FILE = time_str + 'speed.log'
 logging.basicConfig(filename=os.path.join('build', LOGGING_FILE), level=logging.INFO, format='%(asctime)s %(message)s')
 
-def construct(scale=4):
+def construct_two_level_graph(scale=4):
     n = scale * scale
     k1 = scale # inner
     k2 = scale # outer
@@ -68,60 +69,95 @@ def generate_gaussian(num_node):
                 digraph.add_edge(i,j,weight=w)
     return digraph
 
-def generate_and_run_gaussian(num_times):
+def generate_and_run_gaussian(num_times, multi_thread=False):
     gaussian_dic = {}
     for i in GAUSSIAN_NODE_LIST:
         dg = generate_gaussian(i)
         prop = {'num_edge': len(dg.edges)}
         for method in METHOD_LIST:
-            prop[method] = algorithm_runner(method, dg, num_times)
+            prop[method] = algorithm_runner(method, dg, num_times, multi_thread)
         gaussian_dic[i] = prop
     write_json(gaussian_dic, 'gaussian')
 
+def generate_and_run_two_level(num_times, multi_thread=False):
+    two_level_dic = {}
+    for i in TWO_LEVEL_CONFIG_LIST:
+        dg = construct_two_level_graph(i)
+        prop = {'num_edge': len(dg.edges)}
+        for method in METHOD_LIST:
+            prop[method] = algorithm_runner(method, dg, num_times, multi_thread)
+        node_num = len(dg.nodes)
+        two_level_dic[node_num] = prop
+    write_json(two_level_dic, 'two_level')
+    
 def write_json(python_dic, filename_prefix):    
     file_name_path = os.path.join('build', time_str + filename_prefix + '.json')
     with open(file_name_path, 'w') as f:
         st = json.dumps(python_dic, indent=4) # human readable
         f.write(st)
-        
-def algorithm_runner(method_name, digraph, average_times=1):
+
+def task(method_name, digraph, qu):
+    ic = InfoCluster(affinity='precomputed')
+    if(method_name == 'dt'):
+        start_time = time.time()    
+        ic.fit(digraph)
+    elif(method_name == 'pdt'):
+        start_time = time.time()        
+        ic.fit(digraph, use_pdt=True)
+    elif(method_name == 'pdt_r'):
+        start_time = time.time()        
+        ic.fit(digraph, use_pdt_r=True)
+    else:
+        start_time = time.time()        
+        ic.fit(digraph, use_psp_i=True)        
+    end_time = time.time()
+    running_time = end_time - start_time
+    qu.put(running_time)
+            
+def algorithm_runner(method_name, digraph, average_times=1, multi_thread=False):
     '''
         return the time used 
-    '''
-    ic = InfoCluster(affinity='precomputed')
-        
+    '''   
     total_times = 0
-    for i in range(average_times):
-        if(method_name == 'dt'):
-            start_time = time.time()    
-            ic.fit(digraph)
-        elif(method_name == 'pdt'):
-            start_time = time.time()        
-            ic.fit(digraph, use_pdt=True)
-        elif(method_name == 'pdt_r'):
-            start_time = time.time()        
-            ic.fit(digraph, use_pdt_r=True)
-        else:
-            start_time = time.time()        
-            ic.fit(digraph, use_psp_i=True)        
-        end_time = time.time()
-        running_time = end_time - start_time
-        logging.info('{0}, {1}/{2}, node_size: {3}; running time: {4}'.format(method_name, i + 1, 
-            average_times, len(digraph.nodes), running_time))
-
-        total_times += running_time
+    if(multi_thread):
+        from multiprocessing import Process, Queue
+        process_list = []
+        q = Queue()
+        for i in range(average_times):
+            t = Process(target=task, args=(method_name, digraph, q))
+            process_list.append(t)
+            t.start()
+        for i in range(average_times):
+            process_list[i].join()
+            running_time = q.get()
+            total_times += running_time            
+            logging.info('{0}, {1}/{2}, node_size: {3}; running time: {4}'.format(method_name, i + 1, 
+                average_times, len(digraph.nodes), running_time))
+    else:
+        from queue import Queue
+        q = Queue()
+        for i in range(average_times):
+            task(method_name, digraph, q)
+            running_time = q.get()
+            total_times += running_time
+            logging.info('{0}, {1}/{2}, node_size: {3}; running time: {4}'.format(method_name, i + 1, 
+                average_times, len(digraph.nodes), running_time))
         
     return total_times / average_times    
     
 if __name__ == '__main__':
     method_list = ['all'] + METHOD_LIST
-    task_list = ['try', 'gaussian']
+    task_list = ['try', 'gaussian', 'two_level']
     parser = argparse.ArgumentParser()
     parser.add_argument('--node_size', type=int, default=100)
     parser.add_argument('--method', default='all', choices=method_list, nargs='+')
     parser.add_argument('--task', default='try', choices=task_list)
     parser.add_argument('--num_times', type=int, default=1)
+    parser.add_argument('--multi_thread', default=False, nargs='?', const=True)
+    parser.add_argument('--total_times', default=False, nargs='?', const=True, help='compute total running time of the main process')
     args = parser.parse_args()
+    if(args.total_times):
+        start_time = time.time()
     if(args.node_size < 4):
         print('size should be at least 4')
         exit(0)
@@ -135,7 +171,11 @@ if __name__ == '__main__':
             method_inner_list = [args.method]
         print('num of edge', len(dg.edges))
         for method in method_inner_list:
-            time_used = algorithm_runner(method, dg, args.num_times)
+            time_used = algorithm_runner(method, dg, args.num_times, args.multi_thread)
             print(method, time_used)
     elif(args.task == 'gaussian'):
-        generate_and_run_gaussian(args.num_times)
+        generate_and_run_gaussian(args.num_times, args.multi_thread)
+    elif(args.task == 'two_level'):
+        generate_and_run_two_level(args.num_times, args.multi_thread)
+    if(args.total_times):
+        logging.info('Total runing time in seconds: %f'% (time.time() - start_time))
